4821493e7ad620dafe91f8fbe82ef0a773794c95
unknown <akash.tech2511@gmail.com>
2025-11-24 12:18:10 +0530

    updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
 import Header from "@/components/header";
-import Hero from "@/components/hero"; 
+import Hero from "@/components/hero";
+import MessageSection from "@/components/MessageSection"
+import Help from "@/components/help"
+import Services from "@/components/services";
+import AboutMe from "@/components/aboutme";
+
 export default function Home() {
   return (
     <main className="min-h-screen bg-white overflow-x-hidden">
       <Header />
        <Hero/>
-      
+      <MessageSection /> 
+      <Help/>
+      <Services/>
+      <AboutMe/>
     </main>
   );
 }
